d18d8a6fe2923d4b743f6fe085037ee70837b799
BatuhanGUYILDAR <bthn.gyldr01@gmail.com>
2026-05-19 19:30:22 +0300

    Speed Insights eklendi

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { LanguageProvider } from './i18n/LanguageProvider';
 import './index.css';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
         <App />
         <Analytics />
+        <SpeedInsights />
       </BrowserRouter>
     </LanguageProvider>
   </React.StrictMode>,
