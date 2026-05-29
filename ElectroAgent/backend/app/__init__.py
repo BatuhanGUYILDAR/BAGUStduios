e7663cdc08cbaaeba7003801a3c4106f37ed2025
BatuhanGUYILDAR <bthn.gyldr01@gmail.com>
2026-05-30 02:48:38 +0300

    Delete ElectroAgent directory

--- ElectroAgent/backend/app/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Electro Agent backend package."""
--- ElectroAgent/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Window {
-  electro?: {
-    platform: () => Promise<{ platform: string; backendUrl: string }>;
-    onBackendLog: (handler: (line: string) => void) => () => void;
-  };
-}
